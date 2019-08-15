de3baf886107f2bff560dbaf3f199dc6be59e2bd
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-08-15 16:21:30 +0100

    MDTest: Document choice of 42.

--- src/mdtest.c
+++ src/mdtest.c
@@ -700,7 +700,7 @@ void mdtest_read(int random, int dirs, const long dir_iter, char *path) {
 
         /* read file */
         if (read_bytes > 0) {
-            read_buffer[0] = 42;
+            read_buffer[0] = 42; /* use a random value to ensure that the read_buffer is now different from the expected buffer and read isn't sometimes NOOP */
             if (read_bytes != (size_t) backend->xfer (READ, aiori_fh, (IOR_size_t *) read_buffer, read_bytes, &param)) {
                 FAIL("unable to read file %s", item);
             }
