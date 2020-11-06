ad985af76363dabbbc66837fd2aaa00182378149
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-11-06 09:30:59 +0000

    Location problem on non Linux systems.

--- src/utilities.h
+++ src/utilities.h
@@ -30,13 +30,7 @@ extern enum OutputFormat_t outputFormat;  /* format of the output */
  * Try using the system's PATH_MAX, which is what realpath and such use.
  */
 #define MAX_PATHLEN PATH_MAX
-
-
-#ifdef __linux__
 #define ERROR_LOCATION __func__
-#else
-#define ERROR_LOCATION __LINE__
-#endif
 
 
 void* safeMalloc(uint64_t size);
