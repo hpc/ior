8d2fd745d8d4392a545217234490a9427694d26c
Josh Schwartz <52082483+jschwartz-cray@users.noreply.github.com>
2022-02-02 12:24:06 -0700

    Fix #410.
    Added a local variable to the affected macros which ensures the
    parameter is expanded first to prevent multiple calls to any embedded
    functions.

--- src/aiori-debug.h
+++ src/aiori-debug.h
@@ -81,9 +81,10 @@ void FailMessage(int rank, const char *location, char *format, ...);
 #define MPI_CHECKF(MPI_STATUS, FORMAT, ...) do {                        \
     char resultString[MPI_MAX_ERROR_STRING];                            \
     int resultLength;                                                   \
+    int _MPI_STATUS = (MPI_STATUS);                                     \
                                                                         \
-    if (MPI_STATUS != MPI_SUCCESS) {                                    \
-        MPI_Error_string(MPI_STATUS, resultString, &resultLength);      \
+    if (_MPI_STATUS != MPI_SUCCESS) {                                   \
+        MPI_Error_string(_MPI_STATUS, resultString, &resultLength);     \
         fprintf(out_logfile, "ERROR: " FORMAT ", MPI %s, (%s:%d)\n",    \
                 __VA_ARGS__, resultString, __FILE__, __LINE__);         \
         fflush(out_logfile);                                            \
